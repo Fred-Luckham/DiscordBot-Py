5149db7dde762996812cf454925337674fbb68f4
Fred-Luckham <67490061+Fred-Luckham@users.noreply.github.com>
2020-08-26 11:26:23 +0100

    Add files via upload

--- Database Bot/main.py
+++ Database Bot/main.py
@@ -1,8 +1,6 @@
 import discord
 import os
-import time
-import asyncio
-import random
+import logging
 from conf import config
 from discord.ext import commands
 from tinydb import TinyDB, Query
@@ -10,25 +8,32 @@ from tinydb.operations import delete,increment
 
 database = TinyDB('labels.json')
 query = Query()
-# Seperate the token from the main bot
+logger = logging.getLogger(config.logfile)
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename=config.logfile, encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
-# Use '!' as a command prefix
+
 bot = commands.Bot(command_prefix=config.prefix)
 
 # Print the user and guild name upon start up
+# Write to the logfile upon startup 
 @bot.event
 async def on_ready():
     print('------------------------------')
     print('We have logged in as {0}'.format(config.name))
+    print('At server: {0}'.format(config.servers))
     print('------------------------------')
+    logger.info('Logged in as {0} on server: {1}'.format(config.name, config.servers))
     for cog_name in config.cogs:
         try:
             cog_name = 'Cogs.' + cog_name
             bot.load_extension(cog_name)
         except Exception as e:
-            print('Failed to load extension ' + cog_name + ' with error ' + str(e))
+            logger.error('Failed to load extension ' + cog_name + ' with error ' + str(e))
         else:
-            print('Loading extension ' + cog_name)
+            logger.info('Loaded extension ' + cog_name)
 
 
 #If there is an error, it will answer with an error
